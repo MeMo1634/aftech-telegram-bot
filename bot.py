7af955577483eea5fe1baa2f549c0301b14958e5
M.eminKrky <93837642+MeMo1634@users.noreply.github.com>
2025-07-11 22:26:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,22 +8,22 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Merhaba! Ben AF-Tech bilgilendirme botuyum.\nKomutlar:\n/misyon\n/vizyon\n/okul\n/altakimlar\n/basari\n/sosyalmedya")
 
 async def misyon(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("AF-Tech, genç zihinlerin teknolojiye yön verdiği bir öğrenci topluluğudur.")
+    await update.message.reply_text("AF-Tech, genç zihinlerin milli teknoloji hamlesine katkıda bulunduğu, yaratıcı fikirlere yön verdiği bir öğrenci topluluğudur. Topluluk, herhangi bir çıkar gözetmeksizin Milli Teknoloji Hamlesine hizmet eder.")
 
 async def vizyon(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Türkiye’yi teknoloji sahnesinde temsil etmek.")
 
 async def okul(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Baykar Fen Lisesi (Baykar MTAL) öğrencilerinden oluşmaktadır.")
+    await update.message.reply_text("Takımımız, Baykar Fen Lisesi bünyesinde kurulmuştur. Okulumuz Türkiye'nin en iddialı fen liselerinin başında gelmektedir.")
 
 async def altakimlar(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🛩️ Besnek (İHA)\n🤖 Robolig (Otonom Robot)\n🌊 Mucilage ROV")
+    await update.message.reply_text("🛩️ İHA-2: BAYSAL UAV Project (İHA)\n🛩️ İHA-3: SKYWIPE UAV Project (İHA)\n🛩️ AF-SAVAŞAN: TURCOFIGHTER (Savaşan İHA)\n🤖 AF-ASPAN: The Special One (Robolig)\n🌊 AF-TECH Su Altı: TALAY ROV")
 
 async def basari(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🏆 Teknofest Ön Değerlendirme Geçildi\n🏆 ROV Projesi Finalisti")
+    await update.message.reply_text("🏆 3 Robolig takımımız görev videosu aşamasında\n🏆 2 İHA takımımız görev videosu aşamasında")
 
 async def sosyalmedya(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("📷 Instagram: https://instagram.com/aftech.team\n🐦 Twitter: https://twitter.com/aftech_team")
+    await update.message.reply_text("📷 Instagram: https://www.instagram.com/anatolian.falcons/\n🐦 Twitter: https://x.com/tech1AF")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
